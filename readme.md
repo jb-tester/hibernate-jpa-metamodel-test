53f899078bdf1595ae880faad5b4fe79a156951b
irina.petrovskaya <irina.petrovskaya@jetbrains.com>
2023-09-06 19:13:30 +0200

    comments

---
+++ readme.md
@@ -0,0 +1,4 @@
+
+Hibernate: JPA static metamodel generator: support @HQL and @SQL annotations: https://youtrack.jetbrains.com/issue/IDEA-331787 
+
+Hibernate: JPA static metamodel generator: support the @Find annotation: https://youtrack.jetbrains.com/issue/IDEA-331788
--- src/main/java/com/mytests/hibernate/SampleQueries.java
+++ src/main/java/com/mytests/hibernate/SampleQueries.java
@@ -8,12 +8,15 @@ import java.util.List;
 
 public interface SampleQueries {
 
+    // inject Hibernate QL
     @HQL("from Sample where color = :color")
     List<Sample> findSamplesByColor(String color);
 
+    // Inject SQL
     @SQL("select * from sample where color = ?1 and sample = ?2")
     List<Sample> findSamplesByColorAndSample(String color, String sample);
 
+    // the method name doesn't matter but check the parameter names!
     @Find
     List<Sample> dummyMethodName(String sample);
 
